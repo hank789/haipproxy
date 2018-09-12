b719ab4e7c237853ab1f44621d0ffcd333417215
hank <hank.huiwang@gmail.com>
2018-09-12 12:40:12 +0800

    剑鱼360代理ip抓取

---
+++ crawler/validators/jianyu360.py
@@ -0,0 +1,22 @@
+"""
+We use this validator to filter ip that can access mobile zhihu website.
+"""
+from config.settings import (
+    TEMP_JIANYU360_QUEUE, VALIDATED_JIANYU360_QUEUE,
+    TTL_JIANYU360_QUEUE, SPEED_JIANYU360_QUEUE)
+from ..redis_spiders import ValidatorRedisSpider
+from .base import BaseValidator
+
+
+class ZhiHuValidator(BaseValidator, ValidatorRedisSpider):
+    """This validator check the liveness of zhihu proxy resources"""
+    name = 'jianyu360'
+    urls = [
+        'https://www.jianyu360.com/jylab/supsearch/index.html'
+    ]
+    task_queue = TEMP_JIANYU360_QUEUE
+    score_queue = VALIDATED_JIANYU360_QUEUE
+    ttl_queue = TTL_JIANYU360_QUEUE
+    speed_queue = SPEED_JIANYU360_QUEUE
+    success_key = '最新招标信息'
+
